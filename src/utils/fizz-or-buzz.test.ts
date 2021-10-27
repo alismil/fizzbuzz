cb4a8bc0bc2866ab6cd4c28da9c532a55713dfeb
Alisa Milchevskaya <alisa-milchevskaya@hotmail.co.uk>
2021-10-27 18:43:41 +0100

    done app

---
+++ src/utils/fizz-or-buzz.test.ts
@@ -0,0 +1,19 @@
+import { fizzOrBuzz } from "./fizz-or-buzz";
+
+test("should return fizz", () => {
+  expect(fizzOrBuzz(3)).toBe("fizz");
+  expect(fizzOrBuzz(9)).toBe("fizz");
+  expect(fizzOrBuzz(12)).toBe("fizz");
+});
+
+test("should return buzz", () => {
+    expect(fizzOrBuzz(5)).toBe("buzz");
+    expect(fizzOrBuzz(20)).toBe("buzz");
+    expect(fizzOrBuzz(55)).toBe("buzz");
+  });
+
+  test("should return fizz", () => {
+    expect(fizzOrBuzz(1)).toBe("1");
+    expect(fizzOrBuzz(13)).toBe("13");
+    expect(fizzOrBuzz(29)).toBe("29");
+  });
---
+++ src/utils/fizz-or-buzz.ts
@@ -0,0 +1,11 @@
+export function fizzOrBuzz(currentValue: number): number | string {
+    if (currentValue % 3 === 0 && currentValue % 5 === 0) {
+        return 'FizzBuzz'
+    } else if (currentValue % 3 === 0) {
+        return 'Fizz'
+    } else if (currentValue % 5 === 0) {
+        return 'Buzz'
+    } else {
+        return currentValue
+    }
+}
