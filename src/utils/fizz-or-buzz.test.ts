bc3c85661b2654bd91b87779640c6f25632ef854
Alisa Milchevskaya <alisa-milchevskaya@hotmail.co.uk>
2021-10-27 18:48:22 +0100

    added prettier

--- src/utils/fizz-or-buzz.test.ts
+++ src/utils/fizz-or-buzz.test.ts
@@ -7,13 +7,13 @@ test("should return fizz", () => {
 });
 
 test("should return buzz", () => {
-    expect(fizzOrBuzz(5)).toBe("buzz");
-    expect(fizzOrBuzz(20)).toBe("buzz");
-    expect(fizzOrBuzz(55)).toBe("buzz");
-  });
+  expect(fizzOrBuzz(5)).toBe("buzz");
+  expect(fizzOrBuzz(20)).toBe("buzz");
+  expect(fizzOrBuzz(55)).toBe("buzz");
+});
 
-  test("should return fizz", () => {
-    expect(fizzOrBuzz(1)).toBe("1");
-    expect(fizzOrBuzz(13)).toBe("13");
-    expect(fizzOrBuzz(29)).toBe("29");
-  });
+test("should return fizz", () => {
+  expect(fizzOrBuzz(1)).toBe("1");
+  expect(fizzOrBuzz(13)).toBe("13");
+  expect(fizzOrBuzz(29)).toBe("29");
+});
